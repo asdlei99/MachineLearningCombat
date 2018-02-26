8679ad1857d621e91043d11a308f0608f40950ce
wnma3mz <wnma3mz@163.com>
2018-02-26 23:34:47 +0800

    review decisionTreePlot.py

--- 3.DecisionTree/decisionTreePlot.py
+++ 3.DecisionTree/decisionTreePlot.py
@@ -1,15 +1,9 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-'''
-Created on Oct 14, 2010
-Update on 2017-02-27
-Decision Tree Source Code for Machine Learning in Action Ch. 3
-@author: Peter Harrington/jiangzhonglian
-'''
 import matplotlib.pyplot as plt
 
-# 定义文本框 和 箭头格式 【 sawtooth 波浪方框, round4 矩形方框 , fc表示字体颜色的深浅 0.1~0.9 依次变浅，没错是变浅】
+# 定义文本框 和 箭头格式 【 sawtooth 波浪方框, round4 矩形方框 , fc表示字体颜色的深浅 0.1~0.9 依次变浅】
 decisionNode = dict(boxstyle="sawtooth", fc="0.8")
 leafNode = dict(boxstyle="round4", fc="0.8")
 arrow_args = dict(arrowstyle="<-")
@@ -22,10 +16,8 @@ def getNumLeafs(myTree):
     # 根节点开始遍历
     for key in secondDict.keys():
         # 判断子节点是否为dict, 不是+1
-        if type(secondDict[key]) is dict:
-            numLeafs += getNumLeafs(secondDict[key])
-        else:
-            numLeafs += 1
+        numLeafs += getNumLeafs(secondDict[key]) if isinstance(
+            secondDict[key], dict) else 1
     return numLeafs
 
 
@@ -36,29 +28,31 @@ def getTreeDepth(myTree):
     # 根节点开始遍历
     for key in secondDict.keys():
         # 判断子节点是不是dict, 求分枝的深度
-        # ----------写法1 start ---------------
-        if type(secondDict[key]) is dict:
-            thisDepth = 1 + getTreeDepth(secondDict[key])
-        else:
-            thisDepth = 1
-        # ----------写法1 end ---------------
-
-        # ----------写法2 start --------------
-        # thisDepth = 1 + getTreeDepth(secondDict[key]) if type(secondDict[key]) is dict else 1
-        # ----------写法2 end --------------
+        thisDepth = 1 + getTreeDepth(secondDict[key]) if isinstance(
+            secondDict[key], dict) else 1
         # 记录最大的分支深度
         maxDepth = max(maxDepth, thisDepth)
     return maxDepth
 
 
 def plotNode(nodeTxt, centerPt, parentPt, nodeType):
-    createPlot.ax1.annotate(nodeTxt, xy=parentPt,  xycoords='axes fraction', xytext=centerPt, textcoords='axes fraction', va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+    createPlot.ax1.annotate(
+        nodeTxt,
+        xy=parentPt,
+        xycoords='axes fraction',
+        xytext=centerPt,
+        textcoords='axes fraction',
+        va="center",
+        ha="center",
+        bbox=nodeType,
+        arrowprops=arrow_args)
 
 
 def plotMidText(cntrPt, parentPt, txtString):
     xMid = (parentPt[0] - cntrPt[0]) / 2 + cntrPt[0]
     yMid = (parentPt[1] - cntrPt[1]) / 2 + cntrPt[1]
-    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+    createPlot.ax1.text(
+        xMid, yMid, txtString, va="center", ha="center", rotation=30)
 
 
 def plotTree(myTree, parentPt, nodeTxt):
@@ -68,7 +62,8 @@ def plotTree(myTree, parentPt, nodeTxt):
     # depth = getTreeDepth(myTree)
 
     # 找出第1个中心点的位置，然后与 parentPt定点进行划线
-    cntrPt = (plotTree.xOff + (1 + numLeafs) / 2 / plotTree.totalW, plotTree.yOff)
+    cntrPt = (plotTree.xOff + (1 + numLeafs) / 2 / plotTree.totalW,
+              plotTree.yOff)
     # print(cntrPt)
     # 并打印输入对应的文字
     plotMidText(cntrPt, parentPt, nodeTxt)
@@ -82,23 +77,23 @@ def plotTree(myTree, parentPt, nodeTxt):
     plotTree.yOff = plotTree.yOff - 1 / plotTree.totalD
     for key in secondDict.keys():
         # 判断该节点是否是Node节点
-        if type(secondDict[key]) is dict:
+        if isinstance(secondDict[key], dict):
             # 如果是就递归调用[recursion]
             plotTree(secondDict[key], cntrPt, str(key))
         else:
             # 如果不是，就在原来节点一半的地方找到节点的坐标
             plotTree.xOff = plotTree.xOff + 1 / plotTree.totalW
             # 可视化该节点位置
-            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt,
+                     leafNode)
             # 并打印输入对应的文字
             plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
     plotTree.yOff = plotTree.yOff + 1 / plotTree.totalD
 
 
 def createPlot(inTree):
-    # 创建一个figure的模版
     fig = plt.figure(1, facecolor='green')
-    fig.clf()
+    fig.clf()   # 清空
 
     axprops = dict(xticks=[], yticks=[])
     # 表示创建一个1行，1列的图，createPlot.ax1 为第 1 个子图，
@@ -112,26 +107,37 @@ def createPlot(inTree):
     plotTree(inTree, (0.5, 1.0), '')
     plt.show()
 
-
-# # 测试画图
-# def createPlot():
-#     fig = plt.figure(1, facecolor='white')
-#     fig.clf()
-#     # ticks for demo puropses
-#     createPlot.ax1 = plt.subplot(111, frameon=False)
-#     plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
-#     plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
-#     plt.show()
-
-
 # 测试数据集
 def retrieveTree(i):
-    listOfTrees = [
-        {'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
-        {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
-    ]
+    listOfTrees = [{
+        'no surfacing': {
+            0: 'no',
+            1: {
+                'flippers': {
+                    0: 'no',
+                    1: 'yes'
+                }
+            }
+        }
+    }, {
+        'no surfacing': {
+            0: 'no',
+            1: {
+                'flippers': {
+                    0: {
+                        'head': {
+                            0: 'no',
+                            1: 'yes'
+                        }
+                    },
+                    1: 'no'
+                }
+            }
+        }
+    }]
     return listOfTrees[i]
 
 
-# myTree = retrieveTree(1)
-# createPlot(myTree)
+if __name__ == "__main__":
+    myTree = retrieveTree(1)
+    createPlot(myTree)
