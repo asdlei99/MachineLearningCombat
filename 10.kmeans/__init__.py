b91b1870fca3dc4a9c2c7392609e5f1979320a5a
wnma3mz <wnma3mz@163.com>
2018-04-18 15:12:29 +0800

    review kmeans

--- 10.kmeans/__init__.py
+++ 10.kmeans/__init__.py
@@ -2,11 +2,11 @@
 __coding__ = "utf-8"
 __author__ = "Ng WaiMing"
 
-from training.action.unsupervised.kMeans import kMeans
+from . import kMeans
 from numpy import *
 
 if __name__ == '__main__':
-    # dataMat = mat(kMeans.loadDataSet('../../../../data/k-means/testSet.txt'))
+    # dataMat = mat(kMeans.loadDataSet('Data/k-means/testSet.txt'))
     # print('min(dataMat[:, 0])', min(dataMat[:, 0]), '\n')
     # print('min(dataMat[:, 1])', min(dataMat[:, 1]), '\n')
     # print('max(dataMat[:, 0])', max(dataMat[:, 0]), '\n')
@@ -16,9 +16,9 @@ if __name__ == '__main__':
     # centroids, clusterAssment = kMeans.kMeans(dataMat, 4)
     # print('centroids:\n', centroids, '\n')
     # print('clusterAssment:\n',clusterAssment, '\n')
-    # dataMat3 = mat(kMeans.loadDataSet('../../../../data/k-means/testSet2.txt'))
+    # dataMat3 = mat(kMeans.loadDataSet('Data/data/k-means/testSet2.txt'))
     # centList, myNewAssments = kMeans.biKmeans(dataMat3, 3)
     # print('centList: \n', centList, '\n')
-    fileName = '../../../../data/k-means/places.txt'
-    imgName = '../../../../data/k-means/Portland.png'
+    fileName = 'Data/data/k-means/places.txt'
+    imgName = 'Data/data/k-means/Portland.png'
     kMeans.clusterClubs(fileName=fileName, imgName=imgName, numClust=5)
--- 10.kmeans/kMeans.py
+++ 10.kmeans/kMeans.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 __coding__ = "utf-8"
 __author__ = "Ng WaiMing"
-
-from numpy import *
+import numpy as np
 from time import sleep
 import matplotlib
 from matplotlib import pyplot as plt
@@ -14,20 +13,14 @@ def loadDataSet(fileName):
     :param fileName:
     :return:
     '''
-    # 初始化一个空列表
     dataMat = []
-    # 读取文件
-    fr = open(fileName)
-    # 循环遍历文件所有行
-    for line in fr.readlines():
-        # 切割每一行的数据
-        curLine = line.strip().split('\t')
-        # 将数据转换为浮点类型,便于后面的计算
-        # fltLine = [float(x) for x in curLine]
-        # 将数据追加到dataMat
-        fltLine = list(map(float,curLine))    # 映射所有的元素为 float（浮点数）类型
-        dataMat.append(fltLine)
-    # 返回dataMat
+    with open(fileName, 'r') as fr:
+        for line in fr.readlines():
+            # 切割每一行的数据
+            curLine = line.strip().split('\t')
+            # 映射所有的元素为 float（浮点数）类型, 将数据追加到dataMat
+            fltLine = list(map(float, curLine))
+            dataMat.append(fltLine)
     return dataMat
 
 
@@ -38,7 +31,7 @@ def distEclud(vecA, vecB):
     :param vecB:
     :return:
     '''
-    return sqrt(sum(power(vecA - vecB, 2)))
+    return np.sqrt(sum(np.power(vecA - vecB, 2)))
 
 
 def randCent(dataSet, k):
@@ -51,9 +44,9 @@ def randCent(dataSet, k):
     :return:
     '''
     # 获取样本数与特征值
-    m, n = shape(dataSet)
+    _, n = np.shape(dataSet)
     # 初始化质心,创建(k,n)个以零填充的矩阵
-    centroids = mat(zeros((k, n)))
+    centroids = np.mat(np.zeros((k, n)))
     # 循环遍历特征值
     for j in range(n):
         # 计算每一列的最小值
@@ -61,7 +54,7 @@ def randCent(dataSet, k):
         # 计算每一列的范围值
         rangeJ = float(max(dataSet[:, j]) - minJ)
         # 计算每一列的质心,并将值赋给centroids
-        centroids[:, j] = mat(minJ + rangeJ * random.rand(k, 1))
+        centroids[:, j] = np.mat(minJ + rangeJ * np.random.rand(k, 1))
     # 返回质心
     return centroids
 
@@ -77,10 +70,10 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
     :return:
     '''
     # 获取样本数和特征数
-    m, n = shape(dataSet)
+    m, _ = np.shape(dataSet)
     # 初始化一个矩阵来存储每个点的簇分配结果
     # clusterAssment包含两个列:一列记录簇索引值,第二列存储误差(误差是指当前点到簇质心的距离,后面会使用该误差来评价聚类的效果)
-    clusterAssment = mat(zeros((m, 2)))
+    clusterAssment = np.mat(np.zeros((m, 2)))
     # 创建质心,随机K个质心
     centroids = createCent(dataSet, k)
     # 初始化标志变量,用于判断迭代是否继续,如果True,则继续迭代
@@ -90,7 +83,7 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
         # 遍历所有数据找到距离每个点最近的质心,
         # 可以通过对每个点遍历所有质心并计算点到每个质心的距离来完成
         for i in range(m):
-            minDist = inf
+            minDist = np.inf
             minIndex = -1
             for j in range(k):
                 # 计算数据点到质心的距离
@@ -103,14 +96,14 @@ def kMeans(dataSet, k, distMeas=distEclud, createCent=randCent):
             # 如果任一点的簇分配结果发生改变,则更新clusterChanged标志
             if clusterAssment[i, 0] != minIndex: clusterChanged = True
             # 更新簇分配结果为最小质心的index(索引),minDist(最小距离)的平方
-            clusterAssment[i, :] = minIndex, minDist ** 2
+            clusterAssment[i, :] = minIndex, minDist**2
         # print(centroids)
         # 遍历所有质心并更新它们的取值
         for cent in range(k):
             # 通过数据过滤来获得给定簇的所有点
-            ptsInClust = dataSet[nonzero(clusterAssment[:, 0].A == cent)[0]]
+            ptsInClust = dataSet[np.nonzero(clusterAssment[:, 0].A == cent)[0]]
             # 计算所有点的均值,axis=0表示沿矩阵的列方向进行均值计算
-            centroids[cent, :] = mean(ptsInClust, axis=0)
+            centroids[cent, :] = np.mean(ptsInClust, axis=0)
     # 返回所有的类质心与点分配结果
     return centroids, clusterAssment
 
@@ -123,29 +116,31 @@ def biKmeans(dataSet, k, distMeas=distEclud):
     :param distMeas:
     :return:
     '''
-    m, n = shape(dataSet)
+    m, _ = np.shape(dataSet)
     # 创建一个矩阵来存储数据集中每个点的簇分配结果及平方误差
-    clusterAssment = mat(zeros((m, 2)))
+    clusterAssment = np.mat(np.zeros((m, 2)))
     # 计算整个数据集的质心,并使用一个列表来保留所有的质心
-    centroid0 = mean(dataSet, axis=0).tolist()[0]
+    centroid0 = np.mean(dataSet, axis=0).tolist()[0]
     centList = [centroid0]
     # 遍历数据集中所有点来计算每个点到质心的误差值
     for j in range(m):
-        clusterAssment[j, 1] = distMeas(mat(centroid0), dataSet[j, :]) ** 2
+        clusterAssment[j, 1] = distMeas(np.mat(centroid0), dataSet[j, :])**2
     # 对簇不停的进行划分,直到得到想要的簇数目为止
     while (len(centList) < k):
         # 初始化最小SSE为无穷大,用于比较划分前后的SSE
-        lowestSSE = inf
+        lowestSSE = np.inf
         # 通过考察簇列表中的值来获得当前簇的数目,遍历所有的簇来决定最佳的簇进行划分
         for i in range(len(centList)):
             # 对每一个簇,将该簇中的所有点堪称一个小的数据集
-            ptsInCurrCluster = dataSet[nonzero(clusterAssment[:, 0].A == i)[0], :]
+            ptsInCurrCluster = dataSet[np.nonzero(
+                clusterAssment[:, 0].A == i)[0], :]
             # 将ptsInCurrCluster输入到函数kMeans中进行处理,k=2,
             # kMeans会生成两个质心(簇),同时给出每个簇的误差值
             centroidMat, splitClustAss = kMeans(ptsInCurrCluster, 2, distMeas)
             # 将误差值与剩余数据集的误差之和作为本次划分的误差
             sseSplit = sum(splitClustAss[:, 1])
-            sseNotSplit = sum(clusterAssment[nonzero(clusterAssment[:, 0].A != i)[0], 1])
+            sseNotSplit = sum(
+                clusterAssment[np.nonzero(clusterAssment[:, 0].A != i)[0], 1])
             print('sseSplit, and notSplit: ', sseSplit, sseNotSplit)
             # 如果本次划分的SSE值最小,则本次划分被保存
             if (sseSplit + sseNotSplit) < lowestSSE:
@@ -155,9 +150,11 @@ def biKmeans(dataSet, k, distMeas=distEclud):
                 lowestSSE = sseSplit + sseNotSplit
         # 找出最好的簇分配结果
         # 调用kmeans函数并且指定簇数为2时,会得到两个编号分别为0和1的结果簇
-        bestClustAss[nonzero(bestClustAss[:, 0].A == 1)[0], 0] = len(centList)
+        bestClustAss[np.nonzero(
+            bestClustAss[:, 0].A == 1)[0], 0] = len(centList)
         # 更新为最佳质心
-        bestClustAss[nonzero(bestClustAss[:, 0].A == 0)[0], 0] = bestCentToSplit
+        bestClustAss[np.nonzero(
+            bestClustAss[:, 0].A == 0)[0], 0] = bestCentToSplit
         print('the bestCentToSplit is: ', bestCentToSplit)
         print('the len of bestClustAss is: ', len(bestClustAss))
         # 更新质心列表
@@ -166,8 +163,9 @@ def biKmeans(dataSet, k, distMeas=distEclud):
         # 添加bestNewCents的第二个质心
         centList.append(bestNewCents[1, :].tolist()[0])
         # 重新分配最好簇下的数据(质心)以及SSE
-        clusterAssment[nonzero(clusterAssment[:, 0].A == bestCentToSplit)[0], :] = bestClustAss
-    return mat(centList), clusterAssment
+        clusterAssment[np.nonzero(
+            clusterAssment[:, 0].A == bestCentToSplit)[0], :] = bestClustAss
+    return np.mat(centList), clusterAssment
 
 
 def distSLC(vecA, vecB):
@@ -180,10 +178,10 @@ def distSLC(vecA, vecB):
     '''
     # 经度和维度用角度作为单位,但是sin()和cos()以弧度为输入.
     # 可以将江都除以180度然后再诚意圆周率pi转换为弧度
-    a = sin(vecA[0, 1] * pi / 180) * sin(vecB[0, 1] * pi / 180)
-    b = cos(vecA[0, 1] * pi / 180) * cos(vecB[0, 1] * pi / 180) * \
-        cos(pi * (vecB[0, 0] - vecA[0, 0]) / 180)
-    return arccos(a + b) * 6371.0
+    a = np.sin(vecA[0, 1] * np.pi / 180) * np.sin(vecB[0, 1] * np.pi / 180)
+    b = np.cos(vecA[0, 1] * np.pi / 180) * np.cos(vecB[0, 1] * np.pi / 180) * \
+        np.cos(np.pi * (vecB[0, 0] - vecA[0, 0]) / 180)
+    return np.arccos(a + b) * 6371.0
 
 
 def clusterClubs(fileName, imgName, numClust=5):
@@ -200,7 +198,7 @@ def clusterClubs(fileName, imgName, numClust=5):
     for line in open(fileName).readlines():
         lineArr = line.split('\t')
         datList.append([float(lineArr[4]), float(lineArr[3])])
-    datMat = mat(datList)
+    datMat = np.mat(datList)
     # 调用biKmeans并使用distSLC函数作为聚类中使用的距离计算方式
     myCentroids, clustAssing = biKmeans(datMat, numClust, distMeas=distSLC)
     # 创建一幅图和一个举行,使用该矩形来决定绘制图的哪一部分
@@ -218,11 +216,18 @@ def clusterClubs(fileName, imgName, numClust=5):
     ax1 = fig.add_axes(rect, label='ax1', frameon=False)
     # 遍历每一个簇并将它们一一画出来,标记类型从前面创建的scatterMarkers列表中得到
     for i in range(numClust):
-        ptsInCurrCluster = datMat[nonzero(clustAssing[:, 0].A == i)[0], :]
+        ptsInCurrCluster = datMat[np.nonzero(clustAssing[:, 0].A == i)[0], :]
         # 使用索引i % len(scatterMarkers)来选择标记形状,这意味这当有更多簇时,可以循环使用这标记
         markerStyle = scatterMarkers[i % len(scatterMarkers)]
         # 使用十字标记来表示簇中心并在图中显示
-        ax1.scatter(ptsInCurrCluster[:, 0].flatten().A[0], ptsInCurrCluster[:, 1].flatten().A[0], marker=markerStyle,
-                    s=90)
-    ax1.scatter(myCentroids[:, 0].flatten().A[0], myCentroids[:, 1].flatten().A[0], marker='+', s=300)
+        ax1.scatter(
+            ptsInCurrCluster[:, 0].flatten().A[0],
+            ptsInCurrCluster[:, 1].flatten().A[0],
+            marker=markerStyle,
+            s=90)
+    ax1.scatter(
+        myCentroids[:, 0].flatten().A[0],
+        myCentroids[:, 1].flatten().A[0],
+        marker='+',
+        s=300)
     plt.show()
